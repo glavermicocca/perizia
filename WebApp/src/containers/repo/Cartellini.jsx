9cb3d5c11c8727ff3db8ae6ccec990cd2639d4eb
giacomo lavermicocca <giacomo.cervignano@gmail.com>
2020-11-04 08:54:09 +0100

    Ridotto font su data

--- WebApp/src/containers/repo/Cartellini.jsx
+++ WebApp/src/containers/repo/Cartellini.jsx
@@ -179,7 +179,7 @@ const Cartellini = (rows) => {
                 <TextViewValueCenter>L'esemplare qui esaminato e descritto sul retro è garantito autentico</TextViewValueCenter>
                 <Image style={{ marginTop: "4pt", marginBottom: "4pt" }} source={separator}></Image>
                 <Image style={{ width: "51pt" }} source={{ uri: getData(row) }} />
-                <TextViewValue style={{ position: 'absolute', bottom: 48, right: 3 }}>Trieste, {moment(new Date()).format("DD MMMM YYYY")}</TextViewValue>
+                <TextViewValueCaratteristiche style={{ position: 'absolute', bottom: 48, right: 3 }}>Trieste, {moment(new Date()).format("DD MMMM YYYY")}</TextViewValueCaratteristiche>
                 <Image style={{ position: 'absolute', bottom: 25, left: '57pt', right: '0pt' }} source={separator}></Image>
                 <TextViewValue style={{ position: 'absolute', bottom: 15, right: 3 }}>erroridiconiazione.com</TextViewValue>
                 <TextViewValue style={{ position: 'absolute', bottom: 5, right: 3 }}>andreadelpup@libero.it</TextViewValue>
